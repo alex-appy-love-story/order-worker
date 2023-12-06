2c37937ead55a5b38ccc306833cf5c168acecb4d
Alex Simon <aleandre6579.simon@gmail.com>
2023-12-06 14:04:05 +0700

    test: more print

--- tasks/step.go
+++ tasks/step.go
@@ -66,6 +66,7 @@ func Perform(p StepPayload, taskCtx *TaskContext) (err error) {
 
     //NOTE(Alex): Same not as Appy but for default response 
 	// Immediately send back default response if CB is open
+    fmt.Println("Get CB state: ", taskCtx.CircuitBreaker.State())
 	if taskCtx.CircuitBreaker.IsState("open") {
 		err = fmt.Errorf("Default response")
 		taskCtx.TaskFailed(err)
